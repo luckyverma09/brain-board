aa083d941a39e7fc323491e15e286f623bf57f71
Lucky Verma <idonthaveemail1af@gmail.com>
2024-07-25 01:14:52 +0530

    navbar complete

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
 import { ChevronLeft, MenuIcon, PlusCircle, Search, Settings, Plus, Trash } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 import { UserItem } from './user-item';
@@ -14,10 +14,12 @@ import { Item } from './item';
 import { toast } from 'sonner';
 import { DocumentList } from './document-list';
 import { TrashBox } from './trash-box';
+import { Navbar } from './navbar';
 
 export const Navigation = () => {
   const search = useSearch();
   const settings = useSettings();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
   const create = useMutation(api.documents.create);
@@ -164,15 +166,19 @@ export const Navigation = () => {
           isMobile && 'left-0 w-full'
         )}
       >
-        <nav>
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role='button'
-              className='h-6 w-6 text-muted-foreground'
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className=' bg-transparent px-3 py-2 w-full'>
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role='button'
+                className='h-6 w-6 text-muted-foreground'
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
--- convex/documents.ts
+++ convex/documents.ts
@@ -201,3 +201,65 @@ export const getSearch = query({
     return documents;
   },
 });
+
+export const getById = query({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    const document = await ctx.db.get(args.id);
+
+    if (!document) {
+      throw new Error('Document not found');
+    }
+
+    if (document.isPublish && !document.isArchived) {
+      return document;
+    }
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    if (document.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+    return document;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id('documents'),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    converImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublish: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(id);
+
+    if (!existingDocument) {
+      throw new Error('Document not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await ctx.db.patch(args.id, { ...rest });
+    return document;
+  },
+});
