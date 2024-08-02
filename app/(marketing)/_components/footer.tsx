948854bc9e1231ae1a141acef9bfc21c6d66bd41
Lucky Verma <idonthaveemail1af@gmail.com>
2024-08-02 14:55:09 +0530

    some changes

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,17 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
-import { Logo } from "./logo";
+import { Logo } from './logo';
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1f1f1f]">
+    <footer className='flex items-center w-full p-6 bg-background z-50 dark:bg-[#1f1f1f]'>
       <Logo />
-      <div className="md:ml-auto w-full justify-between  md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">
-          Privacy Policy
-        </Button>
-        <Button variant="ghost" size="sm">
-          Terma & Conditions
+      <div className='md:ml-auto w-full justify-between  md:justify-end flex items-center gap-x-2 text-muted-foreground'>
+        <Button variant='ghost' size='sm'>
+          Made By Lucky with ❤️
         </Button>
       </div>
     </footer>
